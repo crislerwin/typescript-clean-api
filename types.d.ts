a68b87817066ce4e86d6de7f19daf5517d2983aa
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-05-07 17:00:55 -0300

    fix:types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+export type TsEslintPluginRules = Record<
+  keyof typeof import("@typescript-eslint/eslint-plugin/dist/configs/all")["rules"],
+  any
+>;
+
+export type EslintRules = import("eslint/lib/options").ESLintRules;
+export type TsEslintRules = EslintRules & TsEslintPluginRules;
+
+export type TsEslintConfig = {
+  rules?: TsEslintRules;
+};
